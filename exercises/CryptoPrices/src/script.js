68a93a2e9007e0e764b443fd0716f49f998198d6
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2025-04-11 09:40:15 +0200

    crypto prices og infinite scroll

--- exercises/CryptoPrices/src/script.js
+++ exercises/CryptoPrices/src/script.js
@@ -1,5 +1,103 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   console.log("Page loaded, JavaScript is running!");
 
   // Add your JS code below here
+  const cryptoTable = document.querySelector(".crypto-data");
+  const backBt = document.querySelector(".backBt");
+  const nextBt = document.querySelector(".nextBt");
+
+  let allData = [];
+  let currentPage = 1;
+  const coinsPerPage = 10;
+  const totalPages = 3;
+
+  // fetch data fra API
+  function fetchData() {
+    const APIurl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false&locale=en";
+
+    fetch(APIurl)
+      .then((response) => response.json())
+      .then((data) => {
+        allData = data;
+        renderTable(currentPage);
+        updateBtns();
+      })
+      .catch((error) => {
+        console.error("Fejl", error);
+        cryptoTable.innerHTML = `<tr><td colspan="3">Fejl med data loading</td></tr>`;
+      });
+  }
+
+  // Render data fra API i table
+  function renderTable(page) {
+    const start = (page - 1) * coinsPerPage;
+    const end = start + coinsPerPage;
+    const visibleCoins = allData.slice(start, end);
+
+    cryptoTable.innerHTML = visibleCoins
+      .map(
+        (coin) =>
+          `
+          <tr>
+          <th scope="row">
+            <div class="coin">
+              <img src=${coin.image} alt=${coin.id}>
+                <span class="coin-txt">
+                  <b class="symbol">${coin.symbol}</b>
+                  <p class="name">${coin.name}</p>
+                </span>
+            </div>
+          </th>
+          
+          <td>
+          <div class="price">
+            <div class="current-price">
+              <b>${coin.current_price.toFixed(2)}</b>
+            </div>
+            <div class="high-low">
+                <span class="price-high-low">H: ${coin.high_24h.toFixed(2)}
+                </span>
+                <span class="price-high-low">L: ${coin.low_24h.toFixed(2)}
+                </span>
+                </div>
+            </div>
+          </td>
+
+        <td class="market">
+          <b>${Intl.NumberFormat("en-US").format(coin.market_cap)}</td></b>
+      </tr>
+      `
+      )
+      .join("");
+  }
+
+  // Updater knapper efter side
+  function updateBtns() {
+    if (currentPage === 1) {
+      backBt.classList.add("disabled");
+    } else {
+      backBt.classList.remove("disabled");
+    }
+
+    if (currentPage === totalPages) {
+      nextBt.classList.add("disabled");
+    } else {
+      nextBt.classList.remove("disabled");
+    }
+  }
+
+  // Knapper skifter side
+  nextBt.addEventListener("click", () => {
+    currentPage++;
+    renderTable(currentPage);
+    updateBtns();
+  });
+
+  backBt.addEventListener("click", () => {
+    currentPage--;
+    renderTable(currentPage);
+    updateBtns();
+  });
+
+  fetchData();
 });
--- exercises/InfiniteScroll/src/script.js
+++ exercises/InfiniteScroll/src/script.js
@@ -1,5 +1,84 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   console.log("Page loaded, JavaScript is running!");
-
   // Add your JS code below here
+
+  const testimonialcontainer = document.querySelector("#testimonial-container");
+
+  let hasNext = true;
+  let afterID = null;
+  let isFetching = false;
+  const limit = 5;
+
+  // Data fetching fra API
+  function fetchData() {
+    // Stop dobelt fetch eller fetch med manglende data
+    if (!hasNext || isFetching) return;
+
+    isFetching = true;
+
+    // Loading SVG
+    testimonialcontainer.innerHTML += `
+    <div class="loading">
+      <svg class="loading-svg" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
+      <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
+      <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
+      </svg>
+    </div>`;
+
+    let apiURL = `https://api.frontendexpert.io/api/fe/testimonials?limit=${limit}`;
+    if (afterID !== null) {
+      apiURL += `&after=${afterID}`;
+    }
+
+    fetch(`https://corsproxy.io/?url=${encodeURIComponent(apiURL)}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const loadingDiv = document.querySelector(".loading");
+        if (loadingDiv) loadingDiv.remove();
+
+        if (data.testimonials.length > 0) {
+          afterID = data.testimonials[data.testimonials.length - 1].id;
+        } else {
+          afterID = null;
+        }
+
+        hasNext = data.hasNext;
+        isFetching = false;
+        renderTestimonials(data.testimonials);
+      })
+      .catch((error) => {
+        console.error("Fejl", error);
+        const loadingDiv = document.querySelector(".loading");
+        if (loadingDiv) loadingDiv.remove();
+        isFetching = false;
+      });
+  }
+
+  // Rendering af testimonials data fra API
+  function renderTestimonials(testimonials) {
+    const testimonialBox = testimonials
+      .map(
+        (testimonial) => `
+          <div class="testimonial">
+            <div>
+              <img class="img" src="https://picsum.photos/65/65?random=${Math.random()}" alt="testimonial picture">
+            </div>
+            <div class="text">
+             <p class="name">${testimonial.id}</p>
+             <p class="message">${testimonial.message}</p>
+            </div>
+          </div>`
+      )
+      .join("");
+    testimonialcontainer.innerHTML += testimonialBox;
+  }
+
+  // Scroll listener => fetcher mere data når man scroller til bunden
+  testimonialcontainer.addEventListener("scroll", () => {
+    if (testimonialcontainer.scrollTop + testimonialcontainer.clientHeight >= testimonialcontainer.scrollHeight - 10) {
+      fetchData();
+    }
+  });
+
+  fetchData();
 });
